1f17a5057255b3beb1e8fc22ab3c3aa0883e2b85
Spanchsan <sanzharab@gmail.com>
2023-04-03 11:22:03 +0600

    Changing IntakeConstants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/IntakeConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/IntakeConstants.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode.drive.opmode;
 
 public class IntakeConstants {
-    public static final double CLOSE_INTAKE = 0.9, OPEN_INTAKE = 0.625,
+    public static final double CLOSE_INTAKE = 0.75, OPEN_INTAKE = 0.625,
             ROTATE_GRAB = 0.2, ROTATE_PEREVOROT = 0.86,
             LIFT_GRAB = 0.1, LIFT_PEREVOROT = 1, LIFT_IDLE = 0.45;
 
